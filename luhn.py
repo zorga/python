339914f8f2091cbc6860752cd265e5f01bafe249
zorga <zorganico@gmail.com>
2014-07-09 19:50:36 +0200

    implementing luhn algorithm

--- luhn.py
+++ luhn.py
@@ -1,6 +1,17 @@
 import sys
 
 def main():
+    try:
+        i_cbn = int(raw_input('Please enter your account number : '))
+    except ValueError:
+        print 'You must enter a number !'
+    s_cbn = str(i_cbn)
+    i = len(s_cbn)-1
+    while i >= 0:
+        #print s_cbn[i]
+        if i%2 != 0:
+            print s_cbn[i]
+        i = i-1
     exit(0)
 
 if __name__ == '__main__':
