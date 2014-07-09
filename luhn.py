80acac73b798f411a33168c4e89426c4c7273889
zorga <zorganico@gmail.com>
2014-07-09 20:19:47 +0200

    luhn is now working

--- luhn.py
+++ luhn.py
@@ -5,14 +5,26 @@ def main():
         i_cbn = int(raw_input('Please enter your account number : '))
     except ValueError:
         print 'You must enter a number !'
+    result = luhn(i_cbn)
+    print result
+
+def luhn(i_cbn):
     s_cbn = str(i_cbn)
-    i = len(s_cbn)-1
+    i = len(s_cbn)-2
+    lsum = int(s_cbn[i+1])
     while i >= 0:
         #print s_cbn[i]
-        if i%2 != 0:
-            print s_cbn[i]
-        i = i-1
-    exit(0)
+        op = int(s_cbn[i])*2
+        if op > 9:
+            tosum = str(op)
+            a = int(tosum[0]) + int(tosum[1])
+            lsum += a
+        else:
+            lsum += op
+        if i-1 >= 0:
+            lsum += int(s_cbn[i-1])
+        i = i-2
+    return lsum
 
 if __name__ == '__main__':
     main()
