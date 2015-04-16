0aa9032f5830ddb6d447d3be749fe80495d3b3d8
zorga <zorganico@gmail.com>
2015-04-16 18:51:07 +0200

    final hackpacket.py

--- hackPacket.py
+++ hackPacket.py
@@ -1,14 +1,35 @@
 import sys
 from scapy.all import *
 import string
+import dpkt
 
 def main():
-    path = '/home/nico/file.pcap'
-    packets = rdpcap(path)
-    f = open('packets.txt', 'w')
-    for p in packets:
-        f.write(str(p.payload))
-    f.close()
+    path = './file.pcap'
+    f = open(path)
+    pcap = dpkt.pcap.Reader(f)
+    udp_n = 0
+    tcp_n = 0
+    total_packet = 0
+    i = 0
+
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        total_packet += 1
+        if eth.type == 2048:
+            ip = eth.data
+            if ip.p == 17:
+                udp_n += 1
+                udp = ip.data
+                payload = udp.data
+                res = open('./packets/packets' + str(i) + '.txt', 'w')
+                res.write(str(payload))
+                res.close()
+            if ip.p == 6:
+                tcp_n += 1
+        i += 1
+    print('> total : ' + str(total_packet))
+    print('> number of udp packets : ' + str(udp_n))
+    print('> number of tcp packets : ' + str(tcp_n))
 
 if __name__ == '__main__':
     main()
