be5937d46923528dda2ab2578f15868bcb10eaad
Jean De Georges <nicolas.ooghe@soprasteria.com>
2016-10-25 17:22:31 +0200

    bam

--- divers/socks.py
+++ divers/socks.py
@@ -1,5 +1,21 @@
+import sys
+
 def main():
-    pass
+    n = int(raw_input().strip())
+    c = map(int, raw_input().strip().split(' '))
+
+    shop = {}
+    count = 0
+
+    for sock in c:
+        if sock not in shop:
+            shop[sock] = 1
+        else:
+            if (shop[sock] % 2 != 0):
+                count = count + 1
+            shop[sock] = shop[sock] + 1
+            
+    print(count)
 
 if __name__ == '__main__':
     main()
