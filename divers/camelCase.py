fd0d2b4da69c4a05653acba3fe1cf08f0087937d
Jean De Georges <nicolas.ooghe@soprasteria.com>
2016-10-25 16:03:32 +0200

    boum

--- divers/camelCase.py
+++ divers/camelCase.py
@@ -1,5 +1,15 @@
+import sys
+
 def main():
-    pass
+    s = raw_input().strip()
+    assert 1 <= len(s) <= 10**5
+
+    words = 1
+    for letter in s:
+        if letter.istitle():
+            words = words + 1
+
+    print(words)
 
 if __name__ == '__main__':
     main()
