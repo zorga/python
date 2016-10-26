33abdabe1a7a29a983bd271b0ace1c95e576f50f
Jean De Georges <nicolas.ooghe@soprasteria.com>
2016-10-26 13:01:09 +0200

    lel

--- divers/sync_shopping.py
+++ divers/sync_shopping.py
@@ -13,30 +13,77 @@ __author__ = "Nicolas Ooghe"
 # all the fish types (collective purchasing)
 big_cat = []
 little_cat = []
+nmk = []
+shop_centers = []
+roads = []
+
 
 import sys
+
 
 def main():
     '''
     The main function of the program
     '''
-    nmk, shopCenters, roads = parseEntry()
+    parsing = parse_entry()
 
-def parseEntry():
+    if not parsing:
+        print("Incorrect user's entries !")
+        return
+
+    # debug
+    print(nmk)
+    print(shop_centers)
+    print(roads)
+
+    # Run graph traversals by the cats
+    big_cat = do_the_shopping(1, nmk[0])
+    little_cat = do_the_shopping(1, nmk[0])
+
+
+def check_list():
+    '''
+    check if the cats have purchased all types
+    of fish
+    '''
+    currentList = big_cat + little_cat
+    return len(set(currentList)) == nmk[2]
+
+
+def do_the_shopping(start, dest):
+    '''
+    function that executes the graph traversal
+    by the big and little cats
+    The most important function !
+    '''
+    currPos = start
+    groceries = []
+
+    # First collect the fish from current position
+     
+    while (start != dest):
+        # decrement currPos because shopping centers indices
+        # begin at 1 and list indices begin at 0
+        groceries.append(shop_centers[currPos - 1][1])
+
+    return groceries 
+
+
+def parse_entry():
     '''
     Parses the data entered by user and check the
     constraints
     '''
+    res = 0
+
     # First line : N M K
     nmk = map(int, raw_input().strip().split(' '))
-    # next N lines : shop center description
-    shopCenters = []
-    # next M lines : roads description
-    roads = []
 
+    # next N lines : shop center description
     for i in range(nmk[0]):
-        shopCenters.append(map(int, raw_input().strip().split(' ')))
+        shop_centers.append(map(int, raw_input().strip().split(' ')))
     
+    # next M lines : roads description
     for i in range(nmk[1]):
         roads.append(map(int, raw_input().strip().split(' ')))
 
@@ -45,7 +92,7 @@ def parseEntry():
     assert 1 <= nmk[1] <= (2 * (10**3)) 
     assert 1 <= nmk[2] <= 10
     
-    for e in shopCenters:
+    for e in shop_centers:
         assert 0 <= e[0] <= nmk[2] 
 
         for aij in e[1:]:
@@ -59,7 +106,8 @@ def parseEntry():
         assert e[0] != e[1]
         assert 1 <= e[2] <= 10**4
     
-    return nmk, shopCenters, roads
+    res = 1
+    return res    
  
 
 if __name__ == '__main__':
