709e2dbf0918462701aefef6d367734aa500c2e3
Jean De Georges <nicolas.ooghe@soprasteria.com>
2016-10-26 10:45:49 +0200

    Graph Theory challenge

---
+++ divers/sync_shopping.py
@@ -0,0 +1,23 @@
+'''
+This is my solution to the hackerrank challenge :
+Synchronous Shopping in the Graph Theory section
+'''
+
+__author__ = "Nicolas Ooghe"
+
+# Define globals here
+
+# the cats are represented using lists showing the
+# types of fish they've already purchased
+# The concatenation of both lists must correspond to
+# all the fish types (collective purchasing)
+big_cat = []
+little_cat = []
+
+import sys
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
