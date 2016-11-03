2eae415c55ad65b28851bceb7c1bf9dc9ff15cf3
zorga <zorganico@gmail.com>
2016-11-03 14:29:14 +0100

    modified fibo

---
+++ divers/modifiedFibo.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+import sys
+
+def main():
+    t1, t2, n = map(int, raw_input().strip().split(' '))
+    assert 0 <= t1 <= 2
+    assert 0 <= t2 <= 2
+    assert 3 <= n <= 20
+
+def fibo(ti, tiplusone):
+    
+
+
+if __name__ == '__main__':
+    main()
