55e1913dad2f3179d615fdd579053a18e883db86
zorga <zorganico@gmail.com>
2016-11-17 14:39:00 +0100

    mdr

--- divers/wordorder_v2.py
+++ divers/wordorder_v2.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python
 
 from collections import *
+import time
 
 def main():
     n = int(raw_input().strip())
-    order = deque()
-    cnt = Counter()
-
-    for i in range(n):
-        w = str(raw_input().strip())
-        cnt[w] += 1
-        if w not in order:
-            order.appendleft(w)
-
-    output = ""
+    words = [raw_input().strip() for i in range(n)]
+    cnt = Counter(words)
     
-    while order:
-        e = order.pop()
-        output += str(cnt[e]) + ' '
-    print(len(cnt))
-    print(output)
+    print len(cnt)
+
+    for word in words:
+        derp = cnt.pop(word, None)
+        if derp == None:
+            continue
+        else:
+            print derp,
 
 if __name__ == '__main__':
     main()
