ad4da1ebe9e3b1720d7d9998f5fa4ee9c4c2ff82
zorga <zorganico@gmail.com>
2016-11-17 11:50:43 +0100

    improvement of minion game

--- divers/minion.py
+++ divers/minion.py
@@ -3,27 +3,68 @@
 import re
 
 def main():
+    # Take user's input
+    s = raw_input().strip()
+    s = s.upper()
+    assert 0 <= len(s) <= 10**6
+    # Current score of the winner
+    winner = 0
+
+    # Computes the sets of voyels and consonents:
     voy = ['A', 'E', 'I', 'O', 'U']
     cons = []
     for i in range(1, 27):
         cons.append(chr(i + 64))
     cons = listdiff(cons, voy)
 
-    s = raw_input().strip()
-    s = s.upper()
-    assert 0 <= len(s) <= 10**6
+    # Case : All letters in 's' are equals:
+    if check_equals(s):
+        tot = score_one_letter(s)
 
-    score_stuart = score(calc_substr_list(s, cons))
-    score_kevin = score(calc_substr_list(s, voy))
-
-    if score_stuart == score_kevin:
-        print("Draw")
-    else:
-        winner = max(score_stuart, score_kevin)
-        if winner == score_stuart:
-            print("Stuart " + str(score_stuart))
+        if s[0] in voy:
+            print("Kevin " + str(tot))
         else:
-            print("Kevin " + str(score_kevin))
+            print("Stuart " + str(tot))
+
+    # Case : All letters in 's' are not equals:
+    else:
+
+        # Computes the scores
+        score_stuart = score(calc_substr_list(s, cons))
+        score_kevin = score(calc_substr_list(s, voy))
+
+        # Print the winner of the game or 'Draw' if draw
+        if score_stuart == score_kevin:
+            print("Draw")
+        else:
+            winner = max(score_stuart, score_kevin)
+            if winner == score_stuart:
+                print("Stuart " + str(score_stuart))
+            else:
+                print("Kevin " + str(score_kevin))
+
+def check_equals(l):
+    '''
+    http://stackoverflow.com/questions/3844801/check-if-all-elements-in-a-list-are-identical
+    '''
+    iterator = iter(l)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+def score_one_letter(s):
+    '''
+    computes the score in the case where all the letters of 's' are
+    equals
+    '''
+    l = len(s)
+    score = 0
+    for i in range(1, l + 1):
+        score = score + i
+    
+    return score
 
 def listdiff(first, second):
     '''
@@ -58,6 +99,7 @@ def calc_substr_list(s, alpha):
                 res.append(s[a:a + i])
         j = j + 1
 
+    print(res)
     return res
 
 def score(substr_list):
@@ -76,12 +118,6 @@ def score(substr_list):
         score = score + score_dict[k]
         
     return score
-
-def kevin(s):
-    '''
-    find all substring starting with voyels in 's'
-    '''
-    return 0
 
 if __name__ == '__main__':
     main()
