e813337ba7d57c365b0b6141ab3f4568c168f9c7
zorga <zorganico@gmail.com>
2016-11-17 14:52:12 +0100

    contest

--- divers/wordorder_v2.py
+++ divers/wordorder_v2.py
@@ -6,11 +6,13 @@ import time
 def main():
     n = int(raw_input().strip())
     words = [raw_input().strip() for i in range(n)]
+    # remove duplicates and count them using Counter collection
     cnt = Counter(words)
     
     print len(cnt)
 
     for word in words:
+        # here we add 'None' arg in case 'word' isn't in the counter anymore
         derp = cnt.pop(word, None)
         if derp == None:
             continue
