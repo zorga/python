16c7d48d0d6cb5a27584a56c294373ffd024582b
zorga <zorganico@gmail.com>
2016-11-23 11:01:50 +0100

    rangoli working !

--- divers/rangoli.py
+++ divers/rangoli.py
@@ -4,16 +4,36 @@ def main():
     n = int(raw_input().strip())
     # 'n' should not be greater than 26 and smaller than 0
     assert 0 < n < 27
+    
+    alphabet = []
+    for i in range(1, n + 1):
+        alphabet.append(chr(i + 96))
 
-    #for i in range(1, n + 1):
-    i = n
-    while i > 0:
-        line = ""
-        print chr(i + 96)
-        i = i - 1
+    rang = ""
 
-def build_line(i):
-    pass
+    for i in range(len(alphabet)):
+        if i <= 0:
+            rang += build_line(alphabet[i:])
+        else:
+            tmp = "" + 2*i*'-' + build_line(alphabet[i:]) + 2*i*'-'
+            rang = tmp + '\n' + rang + '\n' + tmp
+
+    print rang
+
+def build_line(letters):
+    '''
+    build a single line of the rangoli game
+    '''
+    res = "" + letters[0]
+    i = 1
+    
+    while i < len(letters):
+        res = '-' + res + '-'
+        res = letters[i] + res + letters[i]
+        i = i + 1
+
+    return res
+        
         
 
     
