7737830ed48c2d2be32643f901938ecd21e0f76e
zorga <zorganico@gmail.com>
2016-12-06 20:57:09 +0100

    strong password ok

--- divers/strong_password.py
+++ divers/strong_password.py
@@ -8,15 +8,20 @@ Then select a random part of that string of user specified size, as password
 import random
 import argparse
 
-special_chars = ["#", "(", ")", "-", "_", "|", "&", "@", "$", "]", "[", "%"]
-# ASCII chars between including 97 and 122
-# Need uppercase letters as well
+special_chars = [">", "<", "#", "(", ")", "-", "_", "|", "&", "@", "$", "]", "[", "%"]
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("psize", help="Desired password size (between 4 and 20)")
+    parser.add_argument("-s", "--size", help="Desired password size (between 4 and 20)")
     args = parser.parse_args()
-    psize = int(args.psize)
+
+    if not args.size:
+        psize = 12
+        print 'No size submitted : using ' + str(psize) + ' as default.'
+
+    else:
+        psize = int(args.size)
+
     assert 4 <= psize <= 20
     passwd = ""
 
@@ -27,9 +32,12 @@ def main():
         passwd += special_chars[random.randint(0, len(special_chars) - 1)]
         passwd += chr(cUpper)
 
-    rand_i = random.randint(0, len(passwd))
+    rand_i = random.randint(0, len(passwd) - psize)
     rand_j = rand_i + psize
     passwd = passwd[rand_i:rand_j]
+    # shuffle password characters
+    passwd = ''.join(random.sample(passwd, len(passwd)))
+
     print passwd
 
 if __name__ == "__main__":
